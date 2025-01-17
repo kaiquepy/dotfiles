890d1a2f5170647c3284784895bf60d84059b54b
Kaique Vieira <kvieiramiranda@gmail.com>
2025-01-17 12:18:55 -0300

    atualizando o script de instalação

--- install.sh
+++ install.sh
@@ -16,6 +16,9 @@ info() {
   echo -e "\n${BLUE}[INFO]${NC} $1\n"
 }
 
+# Detectar o diretório HOME do usuário que executou o script
+USER_HOME=$(eval echo "~$SUDO_USER")
+
 info "Atualizando o sistema"
 apt-get update && apt-get upgrade -y
 
@@ -39,6 +42,15 @@ apt-get install -y xclip
 
 
 info "Instalando o Docker"
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update &>/dev/null
+
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
@@ -47,7 +59,10 @@ apt-get install -y docker-compose-plugin
 
 
 info "Instalando o Virtual Box"
-apt-get install -y virtualbox
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian bookworm contrib" >> /etc/apt/sources.list
+wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg --dearmor
+sudo apt-get update
+apt-get install -y virtualbox-7.1
 
 if command -v aws &>/dev/null; then
   info "AWS CLI já está instalado. Versão:"
@@ -80,25 +95,34 @@ sudo tar -C /usr/local -xzf "${go_tarball}"
 rm "${go_tarball}"
 
 # Configurar a variável de ambiente PATH
-if ! grep -q '/usr/local/go/bin' ~/.profile; then
-  echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+GOPATH='export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin'
+if ! grep -qxF "$GOPATH" "$USER_HOME/.profile"; then
+  echo "$GOPATH" >> "$USER_HOME/.profile"
 fi
-
-source ~/.profile
 
 
 info "Instalando anew"
-go install -v github.com/tomnomnom/anew@latest
-
+sudo -u "$SUDO_USER" /usr/local/go/bin/go install -v github.com/tomnomnom/anew@latest
 
 info "Instalando o Discord"
 wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
 apt install -y ./discord.deb
 
 
+info "Instalado Google Chrome"
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" -O chrome.deb
+apt install -y ./chrome.deb
+
+
+info "Instalando Spotify"
+curl -sS https://download.spotify.com/debian/pubkey_C85668DF69375001.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
+
 info "Removendo .deb baixados"
 rm *.deb
 
-
-mkdir -p ~/repos/pessoal
-mkdir -p ~/repos/work
+info "Criando diretórios para os repositórios"
+mkdir -p "$USER_HOME/repos/pessoal"
+mkdir -p "$USER_HOME/repos/nuria"
