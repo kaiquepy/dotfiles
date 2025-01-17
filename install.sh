9e30271a2330a2d85d5ed63097a80dba1235f8f9
Kaique Vieira <kvieiramiranda@gmail.com>
2025-01-17 09:39:57 -0300

    atualizando o script de instalação

--- install.sh
+++ install.sh
@@ -19,16 +19,32 @@ info() {
 info "Atualizando o sistema"
 apt-get update && apt-get upgrade -y
 
+
 info "Instalando pacotes essenciais"
-sudo apt-get install -y git curl wget vim unzip jq
+sudo apt-get install -y \
+  git \
+  curl \
+  wget \
+  vim \
+  unzip \
+  coreutils \
+
+
+info "Instalando ferramentas úteis"
+sudo apt-get install -y jq
+
 
 info "Instalando xclip"
 apt-get install -y xclip
+
+
 info "Instalando o Docker"
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+
 info "Instalando o Docker Compose"
 apt-get install -y docker-compose-plugin
+
 
 info "Instalando o Virtual Box"
 apt-get install -y virtualbox
@@ -44,12 +60,41 @@ else
   rm -rf aws awscliv2.zip
 fi
 
+
 info "Instalando PHP"
 apt-get install -y php-common php-cli
+
+
+info "Instalando Go (golang)"
+# Obter a versão mais recente do Go
+latest_version=$(curl -s https://go.dev/dl/?mode=json | jq -r '.[0].version')
+go_tarball="${latest_version}.linux-amd64.tar.gz"
+
+# Baixar o tarball correspondente
+wget "https://go.dev/dl/${go_tarball}"
+
+rm -rf /usr/local/go
+
+# Extrair o tarball para /usr/local
+sudo tar -C /usr/local -xzf "${go_tarball}"
+rm "${go_tarball}"
+
+# Configurar a variável de ambiente PATH
+if ! grep -q '/usr/local/go/bin' ~/.profile; then
+  echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+fi
+
+source ~/.profile
+
+
+info "Instalando anew"
+go install -v github.com/tomnomnom/anew@latest
+
 
 info "Instalando o Discord"
 wget "https://discord.com/api/download?platform=linux&format=deb" -O discord.deb
 apt install -y ./discord.deb
+
 
 info "Removendo .deb baixados"
 rm *.deb
